5f6624bef829de9aea42d0f42e19a9161688d217
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-03-17 23:27:58 -0500

    eliminacion de codigo innecesario

--- call_center.py
+++ call_center.py
@@ -4,32 +4,13 @@ import random
 
 
 def iniciar():
-    mensaje = seleccionar_mensaje()
+    agentes = Llamado_Unico.crear_agentes()
+    unico = Llamado_Unico()
+    unico.agente_disponible(agentes)
+    unico_txt = Llamado_Unico()
+    mensaje = unico_txt.seleccionar_mensaje()
     print(mensaje)
-    agente1 = clasificador.crear_agentes()
-    agente2 = clasificador.crear_agentes()
-    agente3 = clasificador.crear_agentes()
-    agente4 = clasificador.crear_agentes()
-    print(agente1)
-    print(agente2)
-    print(agente3)
-    print(agente4   )
-
-def seleccionar_mensaje() -> str:
-    carpeta = "mensajes"
-    lista_txt = []
-    for archivo in os.listdir(carpeta):
-        if archivo.endswith(".txt"):
-            lista_txt.append(archivo)
     
-    if lista_txt:
-        archivo_aleatorio = random.choice(lista_txt)
-        ruta_completa = os.path.join(carpeta, archivo_aleatorio)
-        file = open(ruta_completa, "r", encoding = "utf-8")
-        contenido = file.read()
-        file.close()
-    return contenido
-
 
 class Agente:
     def __init__(self, id: int, nivel_experiencia: str, estado: str = "Disponible", tiempo_respuesta: int = 0):
@@ -68,17 +49,62 @@ class Queue:
         print(self.queu)
     
 
-class clasificador():
-    def crear_agentes():
-        tipo_experiencias = ["Experto", "Intermedio", "Basico"]
-        id = random.randint(1000, 9999)
-        experiencia = random.randint(0, len(tipo_experiencias) - 1)
-        agente = Agente(id, tipo_experiencias[experiencia])
-        return agente
+class Llamado_Unico:
+    def crear_agentes() -> list:
+        lista_agente = []
+        for _ in range(0, 4):
+            tipo_experiencias = ["Experto", "Intermedio", "Basico"]
+            id = random.randint(1000, 9999)
+            experiencia = random.randint(0, len(tipo_experiencias) - 1)
+            agente = Agente(id, tipo_experiencias[experiencia])
+            lista_agente.append(agente)
+        return lista_agente
+
+    def agente_disponible(self, agentes: list[object]):
+        for i in range(len(agentes)):
+            agente = agentes[i]
+            if agente.estado == "Disponible":
+                print(f"Estoy libre {agente.id}")
+                return agente
     
-    def atender(queue: Queue, agente: Agente):
-        "Aca usar los hilos"
-        pass
+    def seleccionar_mensaje(self) -> str:
+        carpeta = "mensajes"
+        lista_txt = []
+        for archivo in os.listdir(carpeta):
+            if archivo.endswith(".txt"):
+                lista_txt.append(archivo)
+        
+        if lista_txt:
+            archivo_aleatorio = random.choice(lista_txt)
+            ruta_completa = os.path.join(carpeta, archivo_aleatorio)
+            file = open(ruta_completa, "r", encoding = "utf-8")
+            contenido = file.read()
+            file.close()
+        return contenido 
+
+    def palabras_clave(mensaje) -> int:
+        diccionario_clave = {"emergencia": 10, "urgente": 8, "fallo critico": 9, "problema": 5, "consulta": 2, "duda": 1}
+        puntos = 0
+        for claves, valor in diccionario_clave.items():
+            if claves in mensaje:
+                puntos += valor
+        return puntos
+    
+    def porcentaje_experiencia(agente: Agente) -> float:
+        if agente.nivel_experiencia == "Experto":
+            return 0.5
+        elif agente.nivel_experiencia == "Intermedio":
+            return 0.75
+        else:
+            return 1.0
+        
+        
+class Llamado_repetido:
+    def atender(self, agente: Agente, mensaje: str):
+        peso_palabras_claves = Llamado_Unico.palabras_clave(mensaje)
+        factor_nivel = Llamado_Unico.porcentaje_experiencia(agente)
+        agente.tiempo_respuesta = agente.calcular_tiempo(len(mensaje), peso_palabras_claves, factor_nivel)
+
 
 
 
